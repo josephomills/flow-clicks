fda018e457fdff16be7c4fa60b2185eb5391e9d5
Owusu Wilson <owusuwilson980@gmail.com>
2025-06-25 20:26:24 +0000

    change

--- resources/views/livewire/link-group-list.blade.php
+++ resources/views/livewire/link-group-list.blade.php
@@ -52,17 +52,16 @@
                             <!-- Meta Information -->
                             <div class="flex flex-wrap items-center gap-x-4 gap-y-1 text-sm text-muted-foreground">
                                 <span
-                                    class="inline-flex items-center rounded-full bg-muted border px-2.5 py-0.5 text-xs font-semibold gap-1.5">
+                                    class="flex items-center rounded-full bg-muted border px-2.5 py-0.5 text-xs font-semibold gap-1.5">
                                     <x-heroicon-o-user class="h-3 w-3" />
                                     <span>Created By: {{ $group->user->name }}</span>
                                 </span>
                                 <span
-                                    class="inline-flex items-center rounded-full bg-muted border px-2.5 py-0.5 text-xs font-semibold gap-1.5">
+                                    class="flex items-center rounded-full bg-muted border px-2.5 py-0.5 text-xs font-semibold gap-1.5">
                                     <x-heroicon-o-clock class="h-3 w-3" />
                                     <span>Created On: {{ $group->created_at->format('l, F j, Y') }}</span>
                                 </span>
-                              {{-- <span>Role: {{ $group->user->role }}</span>
-                                <span class="sm:hidden">Created {{ $group->created_at->diffForHumans() }}</span> --}}
+              
                             </div>
                         </div>
 
